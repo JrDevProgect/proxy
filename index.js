3fdc661a9a432a459d1a0612203ee241aeea828d
JrDevProgect <jrbusaco2@gmail.com>
2025-10-29 18:32:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const http = require('http');
 const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({});
 const server = http.createServer((req, res) => {
-  proxy.web(req, res, { target: 'url here http' });
+  proxy.web(req, res, { target: 'http://87.106.82.84:12488' });
 });
-server.listen(8080, () => {
+server.listen(12488, () => {
   console.log('Proxy server is running on http://localhost:8080');
 });
